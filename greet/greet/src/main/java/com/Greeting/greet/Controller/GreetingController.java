0624c11977177ba77f9e5b53064fa363273219cf
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-03 19:44:52 +0530

    Added GreetingService

--- greet/greet/src/main/java/com/Greeting/greet/Controller/GreetingController.java
+++ greet/greet/src/main/java/com/Greeting/greet/Controller/GreetingController.java
@@ -1,12 +1,19 @@
 package com.Greeting.greet.Controller;
 
 import org.springframework.web.bind.annotation.*;
+import com.Greeting.greet.service.GreetingService;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+    //This is work for UC1
     @GetMapping
     public Map<String, String> getGreeting() {
         return Map.of("message", "Hello from GET!");
@@ -25,5 +32,26 @@ public class GreetingController {
     @DeleteMapping
     public Map<String, String> deleteGreeting() {
         return Map.of("message", "Hello from DELETE!");
+    }
+    // UC2 Endpoints (Using Service Layer)
+    @GetMapping("/uc2")
+    public Map<String, String> uc2GetGreeting() {
+        return Map.of("message", greetingService.getGreetingMessage());
+    }
+
+    @PostMapping("/uc2")
+    public Map<String, String> uc2PostGreeting(@RequestBody Map<String, String> requestBody) {
+        String name = requestBody.getOrDefault("name", "Guest");
+        return Map.of("message", "Hello, " + name + "is a UC2 POST request.");
+    }
+
+    @PutMapping("/uc2")
+    public Map<String, String> uc2PutGreeting() {
+        return Map.of("message", "from UC2 PUT");
+    }
+
+    @DeleteMapping("/uc2")
+    public Map<String, String> uc2DeleteGreeting() {
+        return Map.of("message", "from UC2 DELETE");
     }
 }
---
+++ greet/greet/src/main/java/com/Greeting/greet/service/GreetingService.java
@@ -0,0 +1,11 @@
+package com.Greeting.greet.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+    public String getGreetingMessage() {
+        return "Hello World from UC2";
+    }
+}
