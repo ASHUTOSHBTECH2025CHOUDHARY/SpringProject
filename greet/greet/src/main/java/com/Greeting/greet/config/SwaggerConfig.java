9789d50d82142feeeb7c4ebdfc3a5143147b47d3
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-05 19:53:19 +0530

    Added Swagger

---
+++ greet/greet/src/main/java/com/Greeting/greet/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.Greeting.greet.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Greeting Service API")
+                        .version("1.0.0")
+                        .description("API for managing and retrieving greeting messages.")
+                        .contact(new Contact()
+                                .name("Ashutosh Choudhary")
+                                .email("rajpalchoudhary732@gmail.com")
+                        )
+                );
+    }
+}
